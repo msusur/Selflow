3d2d997002230cc7a9fe64e009b3c255e143816b
msusur <mail@mertsusur.com>
2013-11-03 21:40:31 +0200

    test project added.

---
+++ Engine.Tests/WorkflowEngineTests.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using Xunit;
+
+namespace Selflow.Engine.Tests
+{
+    public class WorkflowEngineTests
+    {
+        [Fact]
+        public void WorkflowEngineCanExecute()
+        {
+            var wfRepository = new Mock<IWorkflowRepository>();
+            var sessionProvider = new Mock<ISessionProvider>();
+            IWorkflowEngine engine = new WorkflowEngine(wfRepository.Object, sessionProvider.Object);
+
+
+            var result = engine.StartNewInstance("TestFlow", new { id = 0, name = "test" });
+
+            
+        }
+    }
+}
