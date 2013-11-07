5ed0a96d15fd60687f219ad8067abe2c653e3ef7
msusur <mail@mertsusur.com>
2013-11-07 08:27:39 +0200

    Unused string variables fixed.

--- Engine/Exceptions/ElementNotFoundException.cs
+++ Engine/Exceptions/ElementNotFoundException.cs
@@ -7,12 +7,9 @@ namespace Selflow.Engine.Exceptions
     [Serializable]
     public class ElementNotFoundException : Exception
     {
-        private readonly IList<string> _elementNames;
-
         public ElementNotFoundException(IList<string> elementNames)
             : base(string.Format("'{0}' element(s) not found.", elementNames.Aggregate((current, next) => current + "," + next)))
         {
-            _elementNames = elementNames;
         }
     }
 }
